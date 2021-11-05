95f10adf22801cd6b6947f190c5c0c163dc19a18
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-11-05 06:16:31 -0400

    works

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,21 +1,32 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { View, StyleSheet, Text } from 'react-native';
 import GridImageView from 'react-native-grid-image-viewer';
+import { fetchUserPosts } from '../../redux/actions';
 
 export default function Profile() {
+    const dispatch = useDispatch();
     const posts = useSelector(state => state.posts)
+    useEffect(() => {
+        dispatch(fetchUserPosts())
+    }, [posts.length])
     console.log('profile posts,' ,posts)
     let data = posts.map(post => post.downloadURL)
+    const hamURL = 'https://i.postimg.cc/wMH6hyZY/ham1.jpg'
+    // const hamOver = (img) => {
+    //     return (
+            
+    //     )
+    // }
     // data = [...data, 'https://imgur.com/a/yT2LlLG']
-    data = [...data, 'https://imgur.com/a/Q2kJguG']
+    data = [...data]
     console.log(data)
-    const ham = Array(posts.length - 1).fill('ham.png')
+    const ham = Array(posts.length - 1).fill(hamURL)
     return (
         <View style={styles.background}>
             <Text style={styles.headline_text}>Profile</Text>         
                 <GridImageView data={data} />
-                {/* <GridImageView data={ham} /> */}
+                <GridImageView data={ham} style={{position: 'absolute'}} />
         </View>
     )
 }
