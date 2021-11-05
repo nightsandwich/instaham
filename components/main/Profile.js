14122e290990dc877a64d2176f9690d361803f0d
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-11-05 04:38:54 -0400

    can see photos on web but not phone

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { useSelector } from 'react-redux'
+import { View, StyleSheet, Text } from 'react-native';
+import GridImageView from 'react-native-grid-image-viewer';
 
 export default function Profile() {
+    const posts = useSelector(state => state.posts)
+    console.log('profile posts,' ,posts)
+    const data = posts.map(post => post.downloadURL)
     return (
         <View>
-            <Text>Profile</Text>            
+            <Text>Profile</Text>         
+            <GridImageView data={data} />
         </View>
     )
 }
