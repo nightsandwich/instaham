1ac2cb294683d61f8f07ed0242ffce0cd4743e88
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-11-05 06:41:55 -0400

    experimenting with background image

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,6 +1,6 @@
 import React, { useEffect} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image, ImageBackground } from 'react-native';
 import GridImageView from 'react-native-grid-image-viewer';
 import { fetchUserPosts } from '../../redux/actions';
 
@@ -22,12 +22,30 @@ export default function Profile() {
     data = [...data]
     console.log(data)
     const ham = Array(posts.length - 1).fill(hamURL)
+    let opacity = 1;
     return (
-        <View style={styles.background}>
-            <Text style={styles.headline_text}>Profile</Text>         
-                <GridImageView data={data} />
-                <GridImageView data={ham} style={{position: 'absolute'}} />
+        <View style={{justifyContent: 'center',alignItems: 'center'}}>
+
+            <ImageBackground  source={{uri:hamURL}} style={{height: 600,width: 600}}>
+
+                    <View>
+                        <Image
+                            source={ {uri: 'https://firebasestorage.googleapis.com/v0/b/instah…=media&token=619a157e-96d8-4680-a114-9d41bb3a3331'}}
+                            style={{width: 600 , height: 600 ,alignItems: 'center',justifyContent: 'center', opacity: `${opacity}`}} 
+                            onPress={() => {opacity+=.1}}
+                        />
+                    </View>
+
+            </ImageBackground>
+
         </View>
+
+
+        // <View style={styles.background}>
+        //     <Text style={styles.headline_text}>Profile</Text>         
+        //         <GridImageView data={data} />
+        //         {/* <GridImageView data={ham} style={{position: 'absolute'}} /> */}
+        // </View>
     )
 }
 
