8e846056aad171b9e4937b306b88cce7fc391453
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-11-06 00:38:40 -0400

    added reducer and constant for editing user post

--- redux/reducers/posts.js
+++ redux/reducers/posts.js
@@ -1,10 +1,12 @@
 import { FETCH_USER_POSTS } from "../constants"
-
+import { EDIT_OPACITY_POST_USER } from "../constants"
 
 export const postsReducer = (state = [], action) => {
     switch(action.type){
         case FETCH_USER_POSTS:
             return action.posts
+        case EDIT_OPACITY_POST_USER:
+            return state.filter(post => post.id === action.post.id ? action.post : post)
         default:
             return state
     }
