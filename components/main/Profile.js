f025cc058094b73ddc1438a5ecd69c2daff3e3e4
Corinne <83316799+nightsandwich@users.noreply.github.com>
2021-11-05 21:24:12 -0400

    made imagewithham component

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { View, StyleSheet, Text, Image, ImageBackground, Button, TouchableOpacity, PushNotificationIOS } from 'react-native';
+import { View, StyleSheet, Text, Image, ImageBackground, Button, TouchableOpacity } from 'react-native';
 import GridImageView from 'react-native-grid-image-viewer';
 import { fetchUserPosts } from '../../redux/actions';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { FAB } from 'react-native-elements';
 import { Icon } from 'react-native-elements';
+import ImageWithHam from './ImageWithHam';
+
 export default function Profile() {
     const dispatch = useDispatch();
     const [posts, user] = useSelector(state => [state.posts, state.user])
@@ -14,27 +14,8 @@ export default function Profile() {
     }, [posts.length])
     console.log('profile posts,' ,posts)
     let data = posts.map(post => post.downloadURL)
-    // const hamURL = 'https://i.postimg.cc/wMH6hyZY/ham1.jpg'
-    const hamURL = 'https://i.postimg.cc/BQbxBy2b/pngaaa-com-4797789.png'
-    
-    const [opacity, setOpacity] = useState(0);
-    const [opacityPhoto, setOpacityPhoto] = useState(1);
-    // data = [...data]
-    
-    const handleLike = () => {
-      console.log(opacity)
-      setOpacity(opacity + .05);
-      setOpacityPhoto(opacityPhoto - .05);
-    }
+    const image = 'https://i.insider.com/57800f2288e4a77c708b67ad?width=1000&format=jpeg&auto=webp'
 
-    const HamImage = () => {
-      return (
-        <Image
-              source={ {uri: hamURL}}
-              style={{position: 'absolute', width: 300 , height: 300, opacity: `${opacity}`}} 
-          />
-      )
-    }
     return (
       <View style={styles.background}>
         <Text style={styles.headline_text}>Profile
@@ -47,38 +28,12 @@ export default function Profile() {
           Posts: {posts.length}
         </Text> 
         <View>
-          <Image
-              source={ {uri: 'https://i.insider.com/57800f2288e4a77c708b67ad?width=1000&format=jpeg&auto=webp'}}
-              style={{width: 300 , height: 300, opacity: `${opacityPhoto}`}} 
-          />
-          <HamImage/>
-          <FAB 
-            placement='left' 
-            color='black'
-            icon={
-              <Icon name="heart" type='font-awesome'
-                onPress={handleLike}
-                color='red'
-              />
-            }
-          />
+          <ImageWithHam image={image}/>
+          <ImageWithHam image={image}/>
+          {/* <GridImageView data={data} /> */}
+          
         </View>
       </View>
-        // <View style={{justifyContent: 'center',alignItems: 'center'}}>
-
-        //     <ImageBackground  source={{uri:hamURL}} style={{height: 600,width: 600}}>
-
-        //             <View>
-        //                 <Image
-        //                     source={ {uri: 'https://firebasestorage.googleapis.com/v0/b/instah…=media&token=619a157e-96d8-4680-a114-9d41bb3a3331'}}
-        //                     style={{width: 600 , height: 600 ,alignItems: 'center',justifyContent: 'center', opacity: `${opacity}`}} 
-        //                     onPress={() => {opacity+=.1}}
-        //                 />
-        //             </View>
-
-        //     </ImageBackground>
-
-        // </View>
 
 //GRIDIMAGEVIEWER
         // <View style={styles.background}>
